81858d2eb6f5e206d9208e974c470c4c3cb22f4c
DDRitter <sircrovit@gmail.com>
2016-05-22 14:40:27 +0200

    Add background image, english text and final styling

--- app/src/main/java/com/artactivo/fivephobias/MainActivity.java
+++ app/src/main/java/com/artactivo/fivephobias/MainActivity.java
@@ -1,14 +1,20 @@
 package com.artactivo.fivephobias;
 
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    boolean [] isPhobiaVisible = { false, false, false, false, false};
+    boolean[] isPhobiaVisible = {false, false, false, false, false};
+    TextView title;
+    TextView content;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,48 +24,83 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * This method is called when a title is touched
+     * This method is called when a phobia is touched
+     * <p/>
+     * Shows/hides the touched phobia
      *
-     * @param title the view touched
+     * @param view the view touched
      */
-    public void showPhobia(View title) {
-        int phobiaNumber = Integer.parseInt(title.getTag().toString());
-        TextView content;
-        ImageView image;
-        switch (phobiaNumber) {
-            case 0:
-                content = (TextView) findViewById(R.id.content_0);
-                image = (ImageView) findViewById(R.id.image_0);
-                break;
-            case 1:
-                content = (TextView) findViewById(R.id.content_1);
-                image = (ImageView) findViewById(R.id.image_1);
-                break;
-            case 2:
-                content = (TextView) findViewById(R.id.content_2);
-                image = (ImageView) findViewById(R.id.image_2);
-                break;
-            case 3:
-                content = (TextView) findViewById(R.id.content_3);
-                image = (ImageView) findViewById(R.id.image_3);
-                break;
-            default:
-                content = (TextView) findViewById(R.id.content_4);
-                image = (ImageView) findViewById(R.id.image_4);
-                break;
-        }
-        if (content != null && image != null) {
+    public void togglePhobia(View view) {
+        int phobiaNumber = Integer.parseInt(view.getTag().toString());
+        getViewIds(phobiaNumber);
+        if (content != null && image != null && title != null) {
             if (isPhobiaVisible[phobiaNumber]) {
                 content.setVisibility(View.GONE);
-                //image.getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.default_title_height);
-                //title.getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.default_title_height);
+                image.setVisibility(View.GONE);
+                title.setBackgroundColor(ContextCompat.getColor(this, R.color.titleBackground));
+                title.getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.title_height);
                 isPhobiaVisible[phobiaNumber] = false;
             } else {
                 content.setVisibility(View.VISIBLE);
-                //image.getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.expanded_title_height);
-                //title.getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.expanded_title_height);
+                image.setVisibility(View.VISIBLE);
+                title.setBackgroundColor(Color.TRANSPARENT);
+                //title.setHeight(300);  ????
+                //title.setHeight(getResources().getDimensionPixelSize(R.dimen.image_height)); ????
+                title.getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.image_height);
+                hidePhobias();
                 isPhobiaVisible[phobiaNumber] = true;
             }
+        }
+    }
+
+
+    /**
+     * This method hides all the phobias
+     */
+    private void hidePhobias() {
+        for (int i = 0; i <= 4; i++) {
+            if (isPhobiaVisible[i]) {
+                getViewIds(i);
+                content.setVisibility(View.GONE);
+                image.setVisibility(View.GONE);
+                title.setBackgroundColor(ContextCompat.getColor(this, R.color.titleBackground));
+                title.getLayoutParams().height = getResources().getDimensionPixelSize(R.dimen.title_height);
+                isPhobiaVisible[i] = false;
+            }
+        }
+    }
+
+
+    /**
+     * This method gets the proper view ids
+     */
+    private void getViewIds(int viewNumber) {
+        switch (viewNumber) {
+            case 0:
+                title = (TextView) findViewById(R.id.title_0);
+                image = (ImageView) findViewById(R.id.image_0);
+                content = (TextView) findViewById(R.id.content_0);
+                break;
+            case 1:
+                title = (TextView) findViewById(R.id.title_1);
+                image = (ImageView) findViewById(R.id.image_1);
+                content = (TextView) findViewById(R.id.content_1);
+                break;
+            case 2:
+                title = (TextView) findViewById(R.id.title_2);
+                image = (ImageView) findViewById(R.id.image_2);
+                content = (TextView) findViewById(R.id.content_2);
+                break;
+            case 3:
+                title = (TextView) findViewById(R.id.title_3);
+                image = (ImageView) findViewById(R.id.image_3);
+                content = (TextView) findViewById(R.id.content_3);
+                break;
+            default:
+                title = (TextView) findViewById(R.id.title_4);
+                image = (ImageView) findViewById(R.id.image_4);
+                content = (TextView) findViewById(R.id.content_4);
+                break;
         }
     }
 }
